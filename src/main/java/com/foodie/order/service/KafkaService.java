e89010b3b228a88c63320f313c1802c8d621ee24
Chris Tasker <ct307@kent.ac.uk>
2019-09-29 22:10:01 +0100

    update docker-compose

--- src/main/java/com/foodie/order/service/KafkaService.java
+++ src/main/java/com/foodie/order/service/KafkaService.java
@@ -6,32 +6,30 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
 
 public class KafkaService {
-
-  private final KafkaProducer<String, String> kafkaProducer;
+private final Properties kafkaProducerProperties;
 
   public KafkaService(String brokerUrl) {
 
     var serializer = StringSerializer.class.getName();
-    Properties properties = new Properties();
-    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerUrl);
-    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, serializer);
-    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, serializer);
+    kafkaProducerProperties = new Properties();
+    kafkaProducerProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerUrl);
+    kafkaProducerProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, serializer);
+    kafkaProducerProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, serializer);
 
-    this.kafkaProducer = new KafkaProducer<>(properties);
   }
 
   public void publish(String topic, String key, String value) {
+
+    var kafkaProducer = new KafkaProducer<String, String>(kafkaProducerProperties);
+
     ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
 
-    kafkaProducer.send(record, new Callback() {
-      @Override
-      public void onCompletion(RecordMetadata recordMetadata, Exception e){
+    kafkaProducer.send(record, (recordMetadata, e) -> {
 
-        if(e != null) {
-          System.out.println("Error: " + e);
-        } else {
-          System.out.println(recordMetadata);
-        }
+      if(e != null) {
+        System.out.println("Error: " + e);
+      } else {
+        System.out.println(recordMetadata);
       }
     });
 
